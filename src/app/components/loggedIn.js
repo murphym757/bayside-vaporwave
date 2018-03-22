e43052e97b0b349822543ea8129d908a03ca05c7
Maurice Murphy <murphym757@yahoo.com>
2018-03-22 19:46:42 -0400

    added artists param and made styling changes

--- src/app/components/loggedIn.js
+++ src/app/components/loggedIn.js
@@ -13,7 +13,8 @@ export default class LoggedIn extends Component {
                 name: null,
                 album_name: null,
                 release_date: null,
-                image: null
+                image: null,
+                artists_name: null
             }
         }
         if (params.access_token) {
@@ -38,10 +39,14 @@ export default class LoggedIn extends Component {
                         name: "Track " + response.item.track_number + ": " + response.item.name,
                         album_name: "Album: " + response.item.album.name,
                         release_date: "Release Date: " + response.item.album.release_date,
-                        image: response.item.album.images[0].url
+                        image: response.item.album.images[0].url,
+                        artists_name: "Artist: " + response.item.artists[0].name,
                     }
                 });
             })
+    }
+    pauseSong() {
+        spotifyWebApi.searchAlbums()
     }
     render() {
         return <div class="container">
@@ -51,18 +56,22 @@ export default class LoggedIn extends Component {
                         <img class="d-flex align-self-start img-responsive mr-3 rounded col" src={this.state.nowPlaying.image} width="200" />
                         <div class="media-body col">
                             <h3 class="mt-0 track-name">{this.state.nowPlaying.name}</h3>
-                            <h3 class="mt-0 album-name">{this.state.nowPlaying.album_name}</h3>
+                            <a href="/login" class="album-link"><h3 class="mt-0 album-name">{this.state.nowPlaying.album_name}</h3></a>
                             <h3 class="mt-0 release-date">{this.state.nowPlaying.release_date}</h3>
                             <br />
-                            <h3 class="mt-0 track-name">{this.state.nowPlaying.name}</h3>
+                            <h3 class="mt-0 track-name">{this.state.nowPlaying.artists_name}</h3>
                             <iframe src="https://open.spotify.com/embed?uri=spotify:user:chillin757:playlist:6u3fyYjDRVkUcqnDHWE1B6&theme=white" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                            <h3 class="mt-0 artist-name">{this.state.nowPlaying.name}</h3>
+                            <div class="row pt-2">
+                                <a class="col-4 media-button" onClick={() => this.getNowPlaying()}><i class="fas fa-backward"></i></a>
+                                <a class="col-4 media-button" onClick={() => this.pauseSong()}><i class="fas fa-play"></i></a>
+                                <a class="col-4 media-button" onClick={() => this.getNowPlaying()}><i class="fas fa-forward"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <br />
                 <a class="btn btn-vaporwave" onClick={() => this.getNowPlaying()}>Now Playing</a>
-                <a href="/login" class="btn btn-vaporwave" onClick={() => this.getNowPlaying()}><i class="fab fa-spotify"></i>Refresh</a>
+                <a href="/login" class="btn btn-vaporwave" onClick={() => this.getNowPlaying()}><i class="fab fa-spotify"></i> Refresh</a>
             </div>
                 : <a href="/login" class="btn btn-vaporwave" onClick={() => this.getNowPlaying()}><i class="fab fa-spotify"></i> Login</a>
             }
